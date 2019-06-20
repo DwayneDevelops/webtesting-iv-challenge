40b524430f2079584f09df2fa6d533ae352c826f
DwayneDevelops <DwayneDevelops@gmail.com>
2019-06-20 18:09:18 -0500

    still working, need to add test db

--- api/server.js
+++ api/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 
+const Whatever = require('../whatever/whatevermodel.js');
+
 const server = express();
 
 server.use(express.json());
 
 server.get('/', (req, res) => {
-    res.status(200).json({ message: 'Here I go!'});
+    res.status(200).json({ api: 'Here I go!' });
 });
+
+server.get('/whatever', (req, res) => {
+    Whatever.getAll()
+    .then(whatever => {
+        res.status(200).json(whatever);
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    });
+});
+
+module.exports = server;
